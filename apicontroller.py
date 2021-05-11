bff12ad6eb5784840d83d920902bc819924b1345
JaviDiaz189 <83991635+JaviDiaz189@users.noreply.github.com>
2021-05-10 20:15:03 -0500

    Update apicontroller.py

--- apicontroller.py
+++ apicontroller.py
@@ -2,16 +2,16 @@
 #Fecha: 10/05/2021
 #Proyecto: Challenge Mercado Libre
 #Archivo: apicontroller.py
-#Versión 1.0
+#Versión 2.0
 
 from db import get_db
 
 #Definimos la función para insertar  DB
-def insert_info(processor, procactiv, usuarios, os, osv):
+def insert_info(ip, processor, procactiv, usuarios, os, osv):
     db = get_db()
     cursor = db.cursor()
-    statement = "INSERT INTO infoapi(processor, procactiv, usuarios, os, osv) VALUES (?, ?, ?, ?, ?)"
-    cursor.execute(statement, [processor, procactiv, usuarios, os, osv])
+    statement = "INSERT INTO infoapi(ip, processor, procactiv, usuarios, os, osv) VALUES (?, ?, ?, ?, ?, ?)"
+    cursor.execute(statement, [ip, processor, procactiv, usuarios, os, osv])
     db.commit()
     return True
 
@@ -19,6 +19,6 @@ def insert_info(processor, procactiv, usuarios, os, osv):
 def get_info():
     db = get_db()
     cursor = db.cursor()
-    query = "SELECT id, processor, procactiv, usuarios, os, osv FROM infoapi"
+    query = "SELECT id, ip, processor, procactiv, usuarios, os, osv FROM infoapi"
     cursor.execute(query)
     return cursor.fetchall()
